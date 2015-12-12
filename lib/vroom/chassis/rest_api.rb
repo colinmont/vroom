3b0675a80afbfc40026aef63dd17d94180ff04b8
Colin <colinmmontgomery@gmail.com>
2015-12-11 17:46:53 -0700

    completed scopes

--- lib/vroom/chassis/rest_api.rb
+++ lib/vroom/chassis/rest_api.rb
@@ -5,33 +5,26 @@ module Vroom
       require 'httparty'
 
       def user
-        response = HTTParty.get('https://dash.by/api/chassis/v1/user',
-                                :headers => { "Authorization" => "Bearer #{self.access_token}"}
-        )
+        response = HTTParty.get('https://dash.by/api/chassis/v1/user', :headers => { "Authorization" => "Bearer #{self.access_token}"})
         # TODO: validations
         Vroom::Model::User.new(JSON.parse(response.body))
       end
-      
-      def trips
-        # TODO: return TripSet Object
-        response = HTTParty.get('https://dash.by/api/chassis/v1/trips',
-                                headers: { "Authorization" => "Bearer #{self.access_token}"}
-        )
+
+      def trips(startTime, endTime)
+        response = HTTParty.get("https://dash.by/api/chassis/v1/trips?startTime=#{startTime}&endTime=#{endTime}", headers: { "Authorization" => "Bearer #{self.access_token}"})
         JSON.parse(response.body)
       end
 
       def trip(trip_id)
-        response = HTTParty.get("https://dash.by/api/chassis/v1/routes/#{trip_id}",
-                                headers: { "Authorization" => "Bearer #{self.access_token}"}
-        )
+        response = HTTParty.get("https://dash.by/api/chassis/v1/routes/#{trip_id}", headers: { "Authorization" => "Bearer #{self.access_token}"})
         JSON.parse(response.body)
       end
-      
-      def stats
-        # TODO: return Stats Object
-        return 'stats'
+
+      def stats(startTime, endTime)
+        response = HTTParty.get("https://dash.by/api/chassis/v1/stats?startTime=#{startTime}&endTime=#{endTime}", headers: { "Authorization" => "Bearer #{self.access_token}"})
+        JSON.parse(response.body)
       end
-      
+
     end
   end
 end
