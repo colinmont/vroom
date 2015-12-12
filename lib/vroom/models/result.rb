3349539a16bca8863d5c438d6e59f567a59a88cf
Colin <colinmmontgomery@gmail.com>
2015-12-11 21:56:20 -0700

    misc

--- lib/vroom/models/result.rb
+++ lib/vroom/models/result.rb
@@ -1,12 +1,11 @@
 module Vroom
   module Model
     class Result
-      require_relative 'trips'
 
       attr_accessor :result
 
       def initialize(result_obj)
-        @result      = Vroom::Model::Trips.new(result_obj['result'])
+        @result      = result_obj['result']
       end
     end
   end
